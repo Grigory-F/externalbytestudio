6cfc91a8a13b47f681cb3a2db57ec22ad27a910f
Grigory-F <grishka2500@gmail.com>
2024-10-04 14:15:13 +0400

    Инициализация проекта

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+
+let date = useDayjs()
+
+console.log(date(Date.now()).format('DD.MM.YYYY'));
+
+
+</script>
+
